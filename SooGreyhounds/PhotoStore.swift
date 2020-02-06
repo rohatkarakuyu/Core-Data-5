e81b9af61d1984c2a0a8310d031c32eb393d0bde
deenmeefe <eddededeed>
2020-02-06 10:26:27 -0800

    Initial Commit

---
+++ SooGreyhounds/PhotoStore.swift
@@ -0,0 +1,149 @@
+//
+//  PhotoStore.swift
+//  SooGreyhounds
+//
+//  Created by Rohat Karakuyu on 22.01.2020.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
+
+enum PhotosResult {
+    case success([Photo])
+    case failure(Error)
+}
+
+class PhotoStore {
+    
+    let imageStore = ImageStore()
+    
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "SooGreyhounds")
+        container.loadPersistentStores { (description, error) in
+            if let error = error {
+                print("Error setting up Core Data (\(error)).")
+            }
+        }
+        return container
+    }()
+    
+    private let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+    
+    func fetchLatestPhotos(completion: @escaping (PhotosResult) -> Void) {
+    
+        let url = SooGreyhoundsAPI.latestPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            var result = self.processPhotosRequest(data: data, error: error)
+            
+            if case .success = result {
+                do {
+                    try
+                        self.persistentContainer.viewContext.save()
+                } catch let error {
+                    result = .failure(error)
+                }
+            }
+            
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchAllPhotos(completion: @escaping (PhotosResult) -> Void) {
+        let fetchRequest: NSFetchRequest<Photo> = Photo.fetchRequest()
+        let sortByDateTaken = NSSortDescriptor(key: #keyPath(Photo.dateTaken),
+                                               ascending: true)
+        fetchRequest.sortDescriptors = [sortByDateTaken]
+        let viewContext = persistentContainer.viewContext
+        viewContext.perform {
+            do {
+                let allPhotos = try
+                viewContext.fetch(fetchRequest)
+                completion(.success(allPhotos))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        
+        guard
+            let jsonData = data
+        else {
+            return .failure(error!)
+        }
+        
+        return SooGreyhoundsAPI.photos(fromJSON: jsonData, into: persistentContainer.viewContext)
+        
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        
+        guard let photoKey = photo.photoID else {
+            preconditionFailure("Photo expected to have a photoID.")
+        }
+        
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
+        guard let photoURL = photo.remoteURL else {
+            preconditionFailure("Photo expected to have a remote URL.")
+        }
+        let request = URLRequest(url: photoURL as URL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            let result = self.processImageRequest(data: data, error: error)
+            
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+            
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+        let imageData = data,
+        let image = UIImage(data: imageData)
+            else {
+            // Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                }
+                else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+                
+        }
+        return .success(image)
+    }
+    
+    
+}
